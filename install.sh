bb9d61035bd16895ef1a0b8835df5af3be0cce25
Omer Erdinc Yagmurlu <omeryagmurlu@gmail.com>
2025-02-27 00:18:17 +0000

    setup py etc

--- install.sh
+++ install.sh
@@ -8,24 +8,24 @@
 
 
 # The following 4 lines are only for slurm machines. uncomment if needed.  
-export TORCH_CUDA_ARCH_LIST="6.1;6.2;7.0;7.5;8.0"   # a100: 8.0; v100: 7.0; 2080ti: 7.5; titan xp: 6.1
-module purge
-module load cuda/11.1.1
-module load gcc/7.5.0
+# export TORCH_CUDA_ARCH_LIST="6.1;6.2;7.0;7.5;8.0"   # a100: 8.0; v100: 7.0; 2080ti: 7.5; titan xp: 6.1
+# module purge
+# module load cuda/11.1.1
+# module load gcc/7.5.0
 
-# download openpoints
-# git submodule add git@github.com:guochengqian/openpoints.git
-git submodule update --init --recursive
-git submodule update --remote --merge # update to the latest version
+# # download openpoints
+# # git submodule add git@github.com:guochengqian/openpoints.git
+# git submodule update --init --recursive
+# git submodule update --remote --merge # update to the latest version
 
-# install PyTorch
-conda deactivate
-conda env remove --name openpoints
-conda create -n openpoints -y python=3.7 numpy=1.20 numba
-conda activate openpoints
+# # install PyTorch
+# conda deactivate
+# conda env remove --name openpoints
+# conda create -n openpoints -y python=3.7 numpy=1.20 numba
+# conda activate openpoints
 
-# please always double check installation for pytorch and torch-scatter from the official documentation
-conda install -y pytorch=1.10.1 torchvision cudatoolkit=11.3 -c pytorch -c nvidia
+# # please always double check installation for pytorch and torch-scatter from the official documentation
+# conda install -y pytorch=1.10.1 torchvision cudatoolkit=11.3 -c pytorch -c nvidia
 pip install torch-scatter -f https://data.pyg.org/whl/torch-1.10.1+cu113.html
 
 pip install -r requirements.txt
